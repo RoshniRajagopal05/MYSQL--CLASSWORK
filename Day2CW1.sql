dfc9320ac2c2b44c3e284915623cb6e3993c7028
roshni@005 <roshnirajagoapl6@gmail.com>
2025-09-18 18:03:02 +0530

    Add initial student records to the database

---
+++ Day2CW1.sql
@@ -0,0 +1,5 @@
+INSERT INTO `students`(`id`, `name`, `age`, `department`, `grade`) 
+VALUES ('1','Alin','25','IT','75'),
+       ('2','Ashin','26','FINANCE','85'),
+       ('3','Janaki','24','IT','90'),
+       ('4','Soumya','27','FINANCE','73');
